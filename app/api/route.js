24208ae59be74613257b83adf7af01f5d30fcb82
pariitosh <paritoshsagar25@gmail.com>
2024-01-21 16:50:07 +0530

    added cities

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,16 @@
 export async function GET() {
     
-    const data = {'lmao':"ssss"}
+    const data = {
+        Noida: ['Dr. Smith', 'Dr. Johnson', 'Dr. Williams'],
+        Gurgaon: ['Dr. Brown', 'Dr. Davis', 'Dr. Miller'],
+        Chennai: ['Dr. Wilson', 'Dr. Moore', 'Dr. Taylor'],
+        Delhi: ['Dr. Anderson', 'Dr. Thomas', 'Dr. Jackson'],
+        Bangalore: ['Dr. White', 'Dr. Harris', 'Dr. Martin'],
+        Hyderabad: ['Dr. Garcia', 'Dr. Martinez', 'Dr. Robinson'],
+        Pune: ['Dr. Lee', 'Dr. Walker', 'Dr. Hall'],
+        Ghaziabad: ['Dr. Allen', 'Dr. Young', 'Dr. Hernandez'],
+        Faridabad: ['Dr. King', 'Dr. Wright', 'Dr. Hill'],
+        Lucknow: ['Dr. Scott', 'Dr. Green', 'Dr. Adams'],
+         }
     return Response.json({ data })
   }
